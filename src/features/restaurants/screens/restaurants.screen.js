ffb61332141ec478a7bbe7ee38bbd292eebcc744
Muharrem Efe Çayırbahçe <hi@efxlve.com>
2024-10-09 21:22:49 +0300

    🚀Project added

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { StatusBar, SafeAreaView, View } from 'react-native';
+import { StatusBar, SafeAreaView, View, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 
+import { Spacer } from "../../../components/spacer/spacer.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
@@ -14,20 +15,26 @@ const SearchContainer = styled(View)`
     padding: ${props => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled(View)`
-    flex: 1;
-    padding: ${props => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+    contentContainerStyle: {
+        padding: 16,
+    },
+})``;
 
 export const RestaurantsScreen = () => {
-    return ( 
+    return (
         <SafeArea>
             <SearchContainer>
                 <Searchbar />
             </SearchContainer>
-            <RestaurantListContainer>
-                <RestaurantInfoCard />
-            </RestaurantListContainer>
+            <RestaurantList
+                data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }, { name: 6 }, { name: 7 }, { name: 8 }, { name: 9 }, { name: 10 }]}
+                renderItem={() =>
+                    <Spacer position="bottom" size="large">
+                        <RestaurantInfoCard />
+                    </Spacer>}
+                keyExtractor={(item) => item.name}
+            />
         </SafeArea>
     );
 };
